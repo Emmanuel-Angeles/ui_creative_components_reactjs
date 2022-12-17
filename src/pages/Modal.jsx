e25d9cd1204c44b51b43cf4fb5b0d683dfd5973f
Emmanuel-Angeles <eangels1595@gmail.com>
2022-12-17 01:49:57 -0600

    Modificacion de estructura de modal

--- src/pages/Modal.jsx
+++ src/pages/Modal.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import '../styles/App.scss'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClose } from '@fortawesome/free-solid-svg-icons'
 
 const Modal = () => {
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleOpenModal = () => {
+        setOpenModal(!openModal);
+    }
+
     return (
         <div className='containerCenter'>
-            <h1 className='generalTitle'>UI Modal</h1>
+            <button className='button_primary' onClick={handleOpenModal}>Abrir modal</button>
+            <div className={`container_modal ${openModal ? "show_container_modal" : ""}`}>
+                 <div className='modal_overlay' onClick={handleOpenModal}></div>
+                 <button className='modal_button_close' onClick={handleOpenModal}>
+                    <FontAwesomeIcon icon={faClose} />
+                 </button>
+                 <div className='modal_content'>
+                    <h1>Contenido de modal</h1>
+                 </div>
+            </div>
         </div>
     )
 }
